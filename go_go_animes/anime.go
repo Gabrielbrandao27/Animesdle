ff705082f5c3f0bff688c516650c7f59cbd722c3
gabriel <ext.gabriel.brandao@aurenenergia.com.br>
2025-04-11 01:52:00 -0300

    adicionando conexão ao banco de dados e criação de tabelas de animes com insert

--- go_go_animes/anime.go
+++ go_go_animes/anime.go
@@ -1,7 +1,51 @@
 package go_go_animes
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"math/rand"
+)
 
-func PrintAnime(nome string) {
-	fmt.Println("Assistindo:", nome)
+func AnimeCharacter(name string) (string, error) {
+	// If no name was given, return an error with a message.
+	if name == "" {
+		return "", errors.New("empty name")
+	}
+
+	// If a name was given, print it out.
+	character := fmt.Sprintf("The character's name is %s", name)
+
+	return character, nil
+}
+
+func RandomAnimeCharacter() (string, error) {
+	// Generate a random anime character
+	character := randomCharacter()
+
+	// If no character was generated, return an error with a message.
+	if character == "" {
+		return "", errors.New("no character generated")
+	}
+
+	// If a character was generated, print it out.
+	message := fmt.Sprintf("The character's name is %s", character)
+
+	return message, nil
+}
+
+func randomCharacter() string {
+	// List of anime characters
+	characters := []string{
+		"Naruto Uzumaki",
+		"Sasuke Uchiha",
+		"Sakura Haruno",
+		"Monkey D. Luffy",
+		"Zoro Roronoa",
+		"Nami",
+	}
+
+	// Generate a random index
+	index := rand.Intn(len(characters))
+
+	return characters[index]
 }
